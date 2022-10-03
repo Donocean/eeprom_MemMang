287101146b1e717697e9b9ba4f7a2bd8519c2a62
Donocean <1184427366@qq.com>
2022-10-03 20:17:53 +0800

    ver1.0

--- eeprom_MemMang.c
+++ eeprom_MemMang.c
@@ -1,4 +1,137 @@
 #include "eeprom_MemMang.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/* ---------------eeprom空间管理分配图------------------- */
+/* |------------------------------+---------------------| */
+/* | EEPROM片内地址               | 地址中保存的数据    | */
+/* |------------------------------+---------------------| */
+/* | 0x00                         | 保存data0的数据     | */
+/* |------------------------------+---------------------| */
+/* | 0x00 + data0的数据大小       | 保存data1的数据     | */
+/* |------------------------------+---------------------| */
+/* | ...                          |                     | */
+/* |------------------------------+---------------------| */
+/* | EE_MAXADDR - DATA1           | 保存data1的数据大小 | */
+/* |------------------------------+---------------------| */
+/* | EE_MAXADDR - DATA0(枚举类型) | 保存data0的数据大小 | */
+/* |------------------------------+---------------------| */
+
+/**
+ * @brief: 保存一个数据到eeprom
+ * @param: dataAddr 数据的地址
+ * @param: dataSize 数据的大小(可取范围1-255个字节)
+ * @param: dataId   可取范围见.h文件variableLists枚举定义
+ * @usage: 想将 int g_mySensorData; 存储到eeprom
+ *         1. 首先在枚举变量中加入变量名，如头文件所示
+ *         2. 然后调用saveDataToEeprom((void*)&g_mySensorData, sizeof(g_mySensorData), G_MYSENSORDATA);
+ * @retval: 0 写入成功 
+ *          1 写入失败 
+ * 失败的原因:
+ *			1. 写入的数据大小为0
+ *			2. 当前写入的数据之前(variableLists)里有数据没有写入
+ *			3. eeprom剩余空间不够容纳当前写入的数据
+ *			4. 堆申请失败
+ * @warning: 因为数据的偏移地址是根据每个数据大小决定的，因此写入时必须按照variableLists中的顺序从上往下依次写入。
+ */
+unsigned char writeDataToEEprom(void* dataAddr, unsigned char dataSize, variableLists dataId)
+{
+	unsigned int i;
+	unsigned char* tmp = NULL;
+	unsigned int eeDataAddr = 0;
+	unsigned char lastDataSize = 0, currentDataSize = 0;
+	
+	if(dataSize == 0) return 1;
+
+	/* 不能是第一个数据 */
+	if(dataId != 0)
+	{
+		/* 写入前，先检查上一个地址有没有数据写入 */
+		eepromRead((unsigned int)(EE_MAXADDR + dataId - 1), &lastDataSize, 1);
+
+		if(lastDataSize == 0)
+		{
+			return 1;
+		}
+		
+		/* 计算数据写入地址 */
+		tmp = (unsigned char*)malloc(dataId);
+
+		if(tmp != NULL)
+		{
+			eepromRead(EE_MAXADDR - (dataId - 1), tmp, dataId);
+
+			/* 计算写入变量的实际地址 */
+			for(i = 0; i < dataId; i++)
+			{
+				eeDataAddr += tmp[i];
+			}
+
+			free(tmp);
+		}
+		else 
+		{
+			/* 堆申请失败返回*/
+			return 1;
+		}
+	}
+
+	/* 若写入的数据将要覆盖eeprom中的variableLists，直接退出 */
+	if((eeDataAddr + dataSize) > (EE_MAXADDR - dataId))
+	{
+		return 1;
+	}
+
+	/* 获得当前数据大小，如果为0说明是第一次写入 */
+	eepromRead((unsigned int)(EE_MAXADDR - dataId), &currentDataSize, 1);
+
+	if(currentDataSize == 0)
+	{
+		/* 将当前数据大小写入eeprom中的variableLists */
+		eepromWirte((unsigned int)(EE_MAXADDR - dataId), &dataSize, 1);
+	}
+	
+	/* 写入数据 */
+	eepromWirte(eeDataAddr, (unsigned char*)dataAddr, dataSize);
+	
+	return 0;
+}
 
 
+/**
+ * @brief: 保存一个变量到eeprom
+ * @param: dataAddr 数据的地址
+ * @param: dataId   可取范围见.h文件枚举定义
+ * @usage: 想将 int g_mySensorData; 从eeprom，读取到单片机
+ *		   调用readDataFromEEprom((void*)&g_mySensorData, G_MYSENSORDATA)
+ */
+void readDataFromEEprom(void* dataAddr, variableLists dataId)
+{
+	unsigned int i;
+	unsigned char* tmp = NULL;
+	unsigned char dataSize = 0;
+	unsigned int eeDataAddr = 0;
+	
+	/* 获得当前数据大小 */
+	eepromRead((unsigned int)(EE_MAXADDR - dataId), &dataSize, 1);
+
+	/* 申请地址(不能是第一个数据) */
+	if(dataId != 0)
+	{
+		tmp = (unsigned char*)malloc(dataId);
+
+		eepromRead(EE_MAXADDR - (dataId - 1), tmp, dataId);
+
+		/* 计算读取变量的实际地址 */
+		for(i = 0; i < dataId; i++)
+		{
+			eeDataAddr += tmp[i];
+		}
+
+		free(tmp);
+	}
+
+	/* 读取数据 */
+	eepromRead(eeDataAddr, (unsigned char*)dataAddr, dataSize);
+}
 
--- eeprom_MemMang.h
+++ eeprom_MemMang.h
@@ -1,16 +1,34 @@
 #ifndef __EEPROM_MEMMANG_H_
 #define __EEPROM_MEMMANG_H_
 
+/* 用户自己编写eeprom的驱动文件 */
+//#include "24cxx.h" 
 
+/* 用户需将自己读写eeprom的函数填入下宏中 */
+/* eeprom最大容量减一(以字节为单位)，如24C02宏大小为255 */
+#define EE_MAXADDR 
+
+/* 函数类型 void (*) (unsigned int eepromAddr, unsigned char* dataAddr, unsigned char num) */
+#define eepromWirte 
+#define eepromRead  
+
+/* 想保存变量到eeprom时，首先在下面枚举中添加变量名 */
 typedef enum
 {
+	// DATA0,DATA1只是示例，实际使用将17-21行全部删除
 	DATA0,
-
+	DATA1,
+	// 用户将变量名添加到下面
+	G_MYSENSORDATA,
 	
-	DATA0_ADDR,
+
+	// DATA_NUM用于标识eeprom中一共存了多少变量(不允许删改)
+	DATA_NUM,
 }variableLists;
 
 
 
-
+void readDataFromEEprom(void* dataAddr, variableLists dataId);
+unsigned char writeDataToEEprom(void* dataAddr, unsigned char dataSize, variableLists dataId);
 #endif /* __EEPROM_MEMMANG_H_ */
+
